788fe8f25ee595865d02d3c2be3e1e1cfcf6db9d
railwayursin <ursin.stauss@sbb.ch>
2022-08-23 15:20:51 +0200

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SbbModule } from './sbb.module';
 import { ButtonWithIconExample } from './button-with-icon-example';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { RouterModule } from '@angular/router';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [ButtonWithIconExample],
@@ -20,8 +32,20 @@ import { RouterModule } from '@angular/router';
     DragDropModule,
     RouterModule.forRoot([]),
     SbbModule,
+    TranslateModule.forRoot({
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler,
+      },
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [],
+  exports: [TranslateModule],
+  providers: [TranslateService],
   bootstrap: [ButtonWithIconExample],
 })
 export class AppModule {}
--- src/app/button-with-icon-example.ts
+++ src/app/button-with-icon-example.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * @title Button With Icon Example
@@ -9,4 +10,14 @@ import { Component } from '@angular/core';
   templateUrl: 'button-with-icon-example.html',
   styleUrls: ['button-with-icon-example.css'],
 })
-export class ButtonWithIconExample {}
+export class ButtonWithIconExample {
+  constructor(public translate: TranslateService) {
+    this.initTranslation();
+  }
+
+  private initTranslation() {
+    this.translate.addLangs(['en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+}
